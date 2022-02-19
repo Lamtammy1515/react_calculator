fa0e63f42059e6d9f0d9b2bbfb170edc517c1e0c
tammylam <lamtammy1515@gmail.com>
2022-02-18 20:56:44 -0600

    adding new div elements in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="calculator">
+				<div className="display">
+					
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
