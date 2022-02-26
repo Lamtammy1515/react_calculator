1b1ae0201ed0a8224ef2fb33d784b5de6fcaa36c
tammylam <lamtammy1515@gmail.com>
2022-02-26 11:15:32 -0600

    creating operator buttons

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
 				<div className="display">
 					<span>(0)</span>0
 				</div>
-
+					<button>/</button>
+					<button>*</button>
+					<button>+</button>
+					<button>-</button>
 				<div className="operator">
-					
+
 				</div>
 			</div>
 		</div>
